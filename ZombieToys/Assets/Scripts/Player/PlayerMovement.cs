60682b40a9af5ed274ba378331b953dfc9e94cd5
cameronmathies <cmathies@IT103.mr.preble.com>
2015-09-11 12:28:38 -0400

    PLAYER CHARACTER

--- ZombieToys/Assets/Scripts/Player/PlayerMovement.cs
+++ ZombieToys/Assets/Scripts/Player/PlayerMovement.cs
@@ -20,6 +20,44 @@ public class PlayerMovement : MonoBehaviour
 	void FixedUpdate()
 	{
 		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f , v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRootation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRootation);
+
+		}
+
+	}
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f; 
+		anim.SetBool ("IsWalking", walking);
 
 	}
 }
